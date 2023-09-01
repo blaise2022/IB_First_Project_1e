6c7b5877c761d5158f7ffe1e49b71ba5f6f9c9ba
blaise <blaisegm31@gmail.com>
2023-09-01 08:34:29 -0500

    New 1

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("     _____   ");
-        System.out.println("     |   |   ");
-        System.out.println("    -------  ");
-        System.out.println("   |  . .  |");
-        System.out.println("     -----   ");
-        System.out.println("     |---|  ");
-        System.out.println("    |     |");
-        System.out.println("  --       --");
-        System.out.println("  |         |");
-        System.out.println("  |         |");
-        System.out.println("  |         |");
-        System.out.println("  |         |");
+        System.out.println("     _____\n     |   |   \n    -------  \n  _|  . .  |_\n     -----  \n     |---| ");
+        System.out.println("    |_____|");
+        System.out.println(" |--       --|");
+        System.out.println(" ||         ||");
+        System.out.println(" ||         ||");
+        System.out.println(" ||         ||");
+        System.out.println(" ||         ||");
         System.out.println("---         ---");
-        System.out.println("  |         |");
-        System.out.println("  |         |");
+        System.out.println("  |---------|");
+        System.out.println("  |    _    |");
         System.out.println("  |   | |   |");
         System.out.println("  |   | |   |");
         System.out.println("  |   | |   |");
@@ -25,7 +20,7 @@ public class Main {
         System.out.println("  |   | |   |");
         System.out.println("  |   | |   |");
         System.out.println("  |   | |   |");
-        System.out.println("  |   | |   |");
+        System.out.println("  |___| |___|");
         System.out.println("___   | |   ___");
     }
 }
